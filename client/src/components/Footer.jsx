0c553a112a25af830f3a69314792aaf3dfee5dd6
Devnyk <dn8128292@gmail.com>
2025-10-05 13:43:32 +0530

    feat: UI changed of PluginPlay

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
             <div className="flex flex-col gap-2 sm:text-sm max-md:text-xs py-4">
               <p className="font-semibold text-base sm:text-sm">Get in touch</p>
               <p className="cursor-pointer hover:underline">+1-234-567-890</p>
-              <p className="cursor-pointer hover:underline">contact@example.com</p>
+              <p className="cursor-pointer hover:underline">contact@devnyk.com</p>
             </div>
 
           </div>
@@ -42,7 +42,7 @@ const Footer = () => {
       </div>
 
       <p className="text-center sm:text-sm max-md:text-xs py-4 text-gray-400">
-        © {new Date().getFullYear()} QuickShow. All Rights Reserved.
+        © {new Date().getFullYear()} PluginPlay. All Rights Reserved.
       </p>
     </>
   );
